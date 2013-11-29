84c04925e3db7c933bac71603c2253934e7d39bb
JekaKey <jekakey@me.com>
2013-11-29 19:22:59 +0400

    Routine

--- script.js
+++ script.js
@@ -1,50 +1,178 @@
 //Require rtmidi library to recive\send midi message
 var midi = require('midi');
 
+
+var transferArray = new Array;
+
+function add8bitToArray(param) {
+
+	var paramType = typeof(param.value);
+
+	switch (paramType) {
+		case 'number':
+
+
+			transferArray.push(param.value);
+			break;
+		case 'string':
+			for (var i = 0; i < param.size; i++) {
+
+				if (param.value[i])
+					transferArray.push(param.value[i]);
+				else
+					transferArray.push(0);
+
+			}
+			break;
+		case 'boolean':
+			transferArray.push(param.value & 1);
+			break;
+		default:
+			console.error('Unresolve params type: ' + paramType, param);
+			break;
+	}
+
+
+
+}
+
 //Settings for slider,rotator
+
 function slider() {
-	this.active = {value: 1, size: 1};
-	this.reverse = {value: 0, size: 1};
-	this.channel = {value: 0, size: 1};
-	this.event = {value: 0, size: 1};
-	this.min_in_value = {value: 0, size: 2};
-	this.max_in_value = {value: 3000, size: 2};
-	this.min_out_value = {value: 0, size: 1};
-	this.max_out_value = {value: 127, size: 1};
-	this.reserved = {value: 0, size: 5};
+	this.active = {
+		value: 1,
+		size: 1
+	};
+	this.reverse = {
+		value: 0,
+		size: 1
+	};
+	this.channel = {
+		value: 0,
+		size: 1
+	};
+	this.event = {
+		value: 0,
+		size: 1
+	};
+	this.min_in_value = {
+		value: 0,
+		size: 2
+	};
+	this.max_in_value = {
+		value: 3000,
+		size: 2
+	};
+	this.min_out_value = {
+		value: 0,
+		size: 1
+	};
+	this.max_out_value = {
+		value: 127,
+		size: 1
+	};
+	this.reserved = {
+		value: 0,
+		size: 5
+	};
 }
 
 //Setting for pedal
+
 function pedal() {
-	this.active = {value: 1, size: 1};
-	this.behavior = {value: 0, size: 1};
-	this.reverse = {value: 0, size: 1};
-	this.channel = {value: 0, size: 1};
-	this.event = {value: 0, size: 1};
-	this.min_in_value = {value: 0, size: 2};
-	this.max_in_value = {value: 3000, size: 2};
-	this.min_out_value = {value: 0, size: 1};
-	this.max_out_value = {value: 127, size: 1};
-	this.reserved = {value: 0, size: 5};
+	this.active = {
+		value: 1,
+		size: 1
+	};
+	this.behavior = {
+		value: 0,
+		size: 1
+	};
+	this.reverse = {
+		value: 0,
+		size: 1
+	};
+	this.channel = {
+		value: 0,
+		size: 1
+	};
+	this.event = {
+		value: 0,
+		size: 1
+	};
+	this.min_in_value = {
+		value: 0,
+		size: 2
+	};
+	this.max_in_value = {
+		value: 3000,
+		size: 2
+	};
+	this.min_out_value = {
+		value: 0,
+		size: 1
+	};
+	this.max_out_value = {
+		value: 127,
+		size: 1
+	};
+	this.reserved = {
+		value: 0,
+		size: 5
+	};
 }
 
 //Settings for button
+
 function button() {
-	this.type = {value: 1, size: 1};
-	this.behavior = {value: 1, size: 1};
-	this.ccnum = {value: 1, size: 1};
-	this.onvalue = {value: 0, size: 1};
-	this.offvalue = {value: 0, size: 1};
-	this.reserved = {value: 0, size: 5};
+	this.type = {
+		value: 1,
+		size: 1
+	};
+	this.behavior = {
+		value: 1,
+		size: 1
+	};
+	this.ccnum = {
+		value: 1,
+		size: 1
+	};
+	this.onvalue = {
+		value: 0,
+		size: 1
+	};
+	this.offvalue = {
+		value: 0,
+		size: 1
+	};
+	this.reserved = {
+		value: 0,
+		size: 5
+	};
 }
 
 //Preset entity
 var Preset = {
-	id: {value: 1, size: 1},
-	name: {value: 'test', size: 16},
-	midiChannel: {value: 1, size: 1},
-	highResMidiEnable: {value: true, size: 1},
-	analogMidiEnable: {value: true, size: 1},
+	id: {
+		value: 3452,
+		size: 2
+	},
+	name: {
+		value: 'VintageD',
+		size: 16
+	},
+	midiChannel: {
+		value: 1,
+		size: 1
+	},
+	highResMidiEnable: {
+		value: true,
+		size: 1
+	},
+	analogMidiEnable: {
+		value: false,
+		size: 1
+	},
 	pitch: new slider(),
 	modulation: new slider(),
 	pedal1: new pedal(),
@@ -83,7 +211,7 @@ var Preset = {
 };
 
 var midiConnect = {
-	init: function () {
+	init: function() {
 		console.log('await port');
 	},
 	output: new midi.output(),
@@ -124,40 +252,30 @@ var midiConnect = {
 
 // Send a MIDI message.
 
-
-
 window.onload = function() {
 
 	require("nw.gui").Window.get().show();
 
-	var count = 0;
-
 	//Перебираем все свойства пресетов
-	for(var key in Preset) {
+	for (var key in Preset) {
 		if (!Preset.hasOwnProperty(key)) continue;
-		
+
 		var PresValue = Preset[key];
 
-		//Если свойство найдено, то это свойства верхней категории
-		if (PresValue.value) {
-			count = count + PresValue.size;
+		if (PresValue.hasOwnProperty('value')) {
+			add8bitToArray(PresValue);
 		} else {
 
-			for(var key2 in PresValue) {
+			for (var key2 in PresValue) {
 				if (!PresValue.hasOwnProperty(key2)) continue;
-				var PresValue2 = PresValue[key2];
-
-				var test = PresValue2.size
-				count = count + PresValue2.size;
-
+				add8bitToArray(PresValue[key2]);
 			}
 
 		}
 
 	}
 
-	console.log(count);
-
+	console.log(transferArray);
 
 	// midiConnect.init();
 
