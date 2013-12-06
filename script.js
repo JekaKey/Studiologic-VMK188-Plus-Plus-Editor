c56e5e46130f4e6eb3c0041aed97844fcfaa00ab
JekaKey <jekakey@me.com>
2013-12-06 19:07:48 +0400

    routine

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ function add8bitToArray(param) {
 	switch (paramType) {
 		case 'number':
 
+			for (var i = 0; i < param.size; i++) {
+				transferArray.push(param.value);
+			}
 
-			transferArray.push(param.value);
 			break;
 		case 'string':
 			for (var i = 0; i < param.size; i++) {
@@ -31,8 +33,6 @@ function add8bitToArray(param) {
 			console.error('Unresolve params type: ' + paramType, param);
 			break;
 	}
-
-
 
 }
 
@@ -55,14 +55,6 @@ function slider() {
 		value: 0,
 		size: 1
 	};
-	this.min_in_value = {
-		value: 0,
-		size: 2
-	};
-	this.max_in_value = {
-		value: 3000,
-		size: 2
-	};
 	this.min_out_value = {
 		value: 0,
 		size: 1
@@ -73,7 +65,7 @@ function slider() {
 	};
 	this.reserved = {
 		value: 0,
-		size: 5
+		size: 4
 	};
 }
 
@@ -118,7 +110,7 @@ function pedal() {
 	};
 	this.reserved = {
 		value: 0,
-		size: 5
+		size: 4
 	};
 }
 
@@ -154,60 +146,69 @@ function button() {
 //Preset entity
 var Preset = {
 	id: {
+		number: 1,
 		value: 3452,
 		size: 2
 	},
 	name: {
+		number: 2,
 		value: 'VintageD',
 		size: 16
 	},
 	midiChannel: {
+		number: 3,
 		value: 1,
 		size: 1
 	},
 	highResMidiEnable: {
+		number: 4,
 		value: true,
 		size: 1
 	},
 	analogMidiEnable: {
+		number: 5,
 		value: false,
 		size: 1
 	},
-	pitch: new slider(),
-	modulation: new slider(),
-	pedal1: new pedal(),
-	pedal2: new pedal(),
-	pedal3: new pedal(),
-	slider0: new slider(),
-	slider1: new slider(),
-	slider2: new slider(),
-	slider3: new slider(),
-	slider4: new slider(),
-	slider5: new slider(),
-	slider6: new slider(),
-	slider7: new slider(),
-	slider8: new slider(),
-	rotator1: new slider(),
-	rotator2: new slider(),
-	rotator3: new slider(),
-	rotator4: new slider(),
-	rotator5: new slider(),
-	rotator6: new slider(),
-	rotator7: new slider(),
-	rotator8: new slider(),
-	button1: new button,
-	button2: new button,
-	button3: new button,
-	button4: new button,
-	button5: new button,
-	button6: new button,
-	button7: new button,
-	button8: new button,
-	dawbutton1: new button,
-	dawbutton2: new button,
-	dawbutton3: new button,
-	dawbutton4: new button,
-	dawbutton5: new button
+	curve: {
+		value: 123,
+		size: 48
+	},
+	pitch: new slider(6),
+	modulation: new slider(7),
+	pedal1: new pedal(8),
+	pedal2: new pedal(9),
+	pedal3: new pedal(10),
+	slider0: new slider(11),
+	slider1: new slider(12),
+	slider2: new slider(13),
+	slider3: new slider(14),
+	slider4: new slider(15),
+	slider5: new slider(16),
+	slider6: new slider(17),
+	slider7: new slider(18),
+	slider8: new slider(19),
+	rotator1: new slider(20),
+	rotator2: new slider(21),
+	rotator3: new slider(22),
+	rotator4: new slider(23),
+	rotator5: new slider(24),
+	rotator6: new slider(25),
+	rotator7: new slider(26),
+	rotator8: new slider(27),
+	button1: new button(28),
+	button2: new button(29),
+	button3: new button(30),
+	button4: new button(31),
+	button5: new button(32),
+	button6: new button(33),
+	button7: new button(34),
+	button8: new button(35),
+	dawbutton1: new button(36),
+	dawbutton2: new button(37),
+	dawbutton3: new button(38),
+	dawbutton4: new button(39),
+	dawbutton5: new button(40)
 };
 
 var midiConnect = {
@@ -263,7 +264,9 @@ window.onload = function() {
 		var PresValue = Preset[key];
 
 		if (PresValue.hasOwnProperty('value')) {
+
 			add8bitToArray(PresValue);
+
 		} else {
 
 			for (var key2 in PresValue) {
