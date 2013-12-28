8556db444a83897477f4b8ad99a7cbd79ec11afb
JekaKey <jekakey@me.com>
2013-12-28 18:39:38 +0400

    new port init

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //Require rtmidi library to recive\send midi message
-var midi = require('midi');
+var midi = require('midi'),
+	gui = require("nw.gui").Window.get().show();
+
+const portName = 'VMK188++'; //Midi port name
 
 var transferArray = new Array;
 
@@ -93,14 +96,6 @@ function pedal() {
 		value: 0,
 		size: 1
 	};
-	this.min_in_value = {
-		value: 0,
-		size: 2
-	};
-	this.max_in_value = {
-		value: 3000,
-		size: 2
-	};
 	this.min_out_value = {
 		value: 0,
 		size: 1
@@ -147,74 +142,133 @@ function button() {
 //Preset entity
 var Preset = {
 	id: {
-		number: 1,
 		value: 3452,
 		size: 2
 	},
 	name: {
-		number: 2,
 		value: 'VintageD',
 		size: 16
 	},
 	midiChannel: {
-		number: 3,
 		value: 1,
 		size: 1
 	},
 	highResMidiEnable: {
-		number: 4,
 		value: true,
 		size: 1
 	},
 	analogMidiEnable: {
-		number: 5,
 		value: false,
+		size: 1
+	},
+	curveUnique: {
+		value: true,
+		size: 1
+	},
+	curveId: {
+		value: 0,
 		size: 1
 	},
 	curve: {
 		value: 123,
 		size: 48
 	},
-	pitch: new slider(6),
-	modulation: new slider(7),
-	pedal1: new pedal(8),
-	pedal2: new pedal(9),
-	pedal3: new pedal(10),
-	slider0: new slider(11),
-	slider1: new slider(12),
-	slider2: new slider(13),
-	slider3: new slider(14),
-	slider4: new slider(15),
-	slider5: new slider(16),
-	slider6: new slider(17),
-	slider7: new slider(18),
-	slider8: new slider(19),
-	rotator1: new slider(20),
-	rotator2: new slider(21),
-	rotator3: new slider(22),
-	rotator4: new slider(23),
-	rotator5: new slider(24),
-	rotator6: new slider(25),
-	rotator7: new slider(26),
-	rotator8: new slider(27),
-	button1: new button(28),
-	button2: new button(29),
-	button3: new button(30),
-	button4: new button(31),
-	button5: new button(32),
-	button6: new button(33),
-	button7: new button(34),
-	button8: new button(35),
-	dawbutton1: new button(36),
-	dawbutton2: new button(37),
-	dawbutton3: new button(38),
-	dawbutton4: new button(39),
-	dawbutton5: new button(40)
+	split1_key: {
+		value: 0,
+		size: 1
+	},
+	split1_offset: {
+		value: 0,
+		size: 1
+	},
+	split2_key: {
+		value: 0,
+		size: 1
+	},
+	split2_offset: {
+		value: 0,
+		size: 1
+	},
+	pitch: new slider(),
+	modulation: new slider(),
+	pedal1: new pedal(),
+	pedal2: new pedal(),
+	pedal3: new pedal(),
+	slider0: new slider(),
+	slider1: new slider(),
+	slider2: new slider(),
+	slider3: new slider(),
+	slider4: new slider(),
+	slider5: new slider(),
+	slider6: new slider(),
+	slider7: new slider(),
+	slider8: new slider(),
+	rotator1: new slider(),
+	rotator2: new slider(),
+	rotator3: new slider(),
+	rotator4: new slider(),
+	rotator5: new slider(),
+	rotator6: new slider(),
+	rotator7: new slider(),
+	rotator8: new slider(),
+	button1: new button(),
+	button2: new button(),
+	button3: new button(),
+	button4: new button(),
+	button5: new button(),
+	button6: new button(),
+	button7: new button(),
+	button8: new button(),
+	dawbutton1: new button(),
+	dawbutton2: new button(),
+	dawbutton3: new button(),
+	dawbutton4: new button(),
+	dawbutton5: new button()
 };
+
+var functionInvmk = {};
+
+functionInvmk['id'] = 100;
+functionInvmk['name'] = 101;
+functionInvmk['midiChannel'] = 102;
+functionInvmk['highResMidiEnable'] = 103;
+functionInvmk['analogMidiEnable'] = 104;
+
 
 var midiConnect = {
 	init: function() {
 		console.log('await port');
+
+		var response = false;
+
+		var inputCount = this.input.getPortCount(),
+			outputCount = this.output.getPortCount();
+
+		for (var i = 0; i < inputCount; i++) {
+			var curPortName = this.input.getPortName(i);
+
+			if (curPortName === portName) {
+				this.input.openPort(i);
+				response = true;
+				console.log('Open input: ' + curPortName);
+			}
+
+		}
+
+		for (var i = 0; i < outputCount; i++) {
+			var curPortName = this.output.getPortName(i);
+
+			if (curPortName === portName) {
+				this.output.openPort(i);
+				response = true;
+				console.log('Output input: ' + curPortName);
+			}
+
+		}
+
+		this.input.ignoreTypes(false, false, false);
+
+		return response;
 	},
 	output: new midi.output(),
 	input: new midi.input()
@@ -247,18 +301,18 @@ function sendData(command, data) {
 	}
 
 	//2 byte command
-	var command1 = (command & 0xF000)>>12,
-		command2 = (command & 0xF00)>>8,
-		command3 = (command & 0xF0)>>4,
+	var command1 = (command & 0xF000) >> 12,
+		command2 = (command & 0xF00) >> 8,
+		command3 = (command & 0xF0) >> 4,
 		command4 = command & 0xF;
 
 	sendArray.push([0xF0, command1, command2]);
 	sendArray.push([0xF0, command3, command4]);
 
-	// //1 byte data syze
+	//1 byte data syze
 	sendArray.push([0xF0, 0, 4]);
 
-	// //Data
+	//Data
 	sendArray.push([0xF0, 0, 1]);
 	sendArray.push([0xF0, 0, 2]);
 	sendArray.push([0xF0, 0, 3]);
@@ -267,20 +321,17 @@ function sendData(command, data) {
 	//End message action
 	sendArray.push([0xF0, 1, 0xF7]);
 
-	sendSysexMessages();
+	// sendSysexMessages();
 
 }
 
 window.onload = function() {
 
-	require("nw.gui").Window.get().show();
-
-	midiConnect.init();
-
-	console.log('Port name: ' + midiConnect.output.getPortName(0));
-
-	midiConnect.output.openPort(0);
-	midiConnect.input.openPort(0);
+	if (midiConnect.init()) {
+		console.log('Port init success');
+	} else {
+		alert('Port VMK188++ not found!');
+	}
 
 	midiConnect.input.ignoreTypes(false, false, false);
 
@@ -294,28 +345,6 @@ window.onload = function() {
 		console.log('m:' + message + ' d:' + deltaTime);
 
 	});
-
-	//Перебираем все свойства пресетов
-	for (var key in Preset) {
-		if (!Preset.hasOwnProperty(key)) continue;
-
-		var PresValue = Preset[key];
-
-		if (PresValue.hasOwnProperty('value')) {
-
-			add8bitToArray(PresValue);
-
-		} else {
-
-			for (var key2 in PresValue) {
-				if (!PresValue.hasOwnProperty(key2)) continue;
-				add8bitToArray(PresValue[key2]);
-			}
-
-		}
-
-	}
-
 
 	// Add listners
 	$('#led1').on('click', function() {
@@ -331,6 +360,37 @@ window.onload = function() {
 		sendData(4, [1, 2, 3, 4]);
 	});
 
+
+	$('#presetUpload').on('click', function() {
+		//Перебираем все свойства пресетов
+		for (var key in Preset) {
+			if (!Preset.hasOwnProperty(key)) continue;
+
+			var PresValue = Preset[key];
+
+			if (functionInvmk[key] > 0) {
+
+				console.log('yes');
+
+			}
+
+			if (PresValue.hasOwnProperty('value')) {
+
+				add8bitToArray(PresValue);
+
+			} else {
+
+				for (var key2 in PresValue) {
+					if (!PresValue.hasOwnProperty(key2)) continue;
+					add8bitToArray(PresValue[key2]);
+				}
+
+			}
+
+		}
+
+
+	});
 
 
 }
